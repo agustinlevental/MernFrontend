4aaafb3ed76882bf6af0487845d222362a196291
Agustin <agustinlevental@gmail.com>
2024-10-18 17:19:42 -0300

    enrutamiento

--- src/router/AdminRouter.jsx
+++ src/router/AdminRouter.jsx
@@ -1,8 +1,7 @@
-import { Auth, Users ,Blog} from "../pages/admin";
+import { Auth, Users, Blog, Courses, Menu, Newsletter } from "../pages/admin";
 import { Routes, Route } from "react-router-dom";
 import { AdminLayout } from "../layout";
 import { Fragment } from "react";
-
 
 const user = 1;
 
@@ -20,10 +19,23 @@ export function AdminRouter() {
         <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
       ) : (
         <Fragment>
-{["/admin","/admin/blog"].map((path)=>(
-  <Route key={path} path={path} element={loadLayout(AdminLayout,Blog)}/>
-))}
+          {["/admin", "/admin/blog"].map((path) => (
+            <Route
+              key={path}
+              path={path}
+              element={loadLayout(AdminLayout, Blog)}
+            />
+          ))}
           <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
+          <Route
+            path="/admin/courses"
+            element={loadLayout(AdminLayout, Courses)}
+          />
+          <Route path="/admin/menu" element={loadLayout(AdminLayout, Menu)} />
+          <Route
+            path="/admin/newsletter"
+            element={loadLayout(AdminLayout, Newsletter)}
+          />
         </Fragment>
       )}
     </Routes>
