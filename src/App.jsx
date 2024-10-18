6322584fb932207b6fba1c9ece5bcc89cb6a14d3
Agustin <agustinlevental@gmail.com>
2024-10-18 15:55:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'semantic-ui-react'
+
+export default function App() {
+  return (
+    <div>
+  <h1>estamos en mi web</h1>
+  <div>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+  </div>
+  </div>
+  )
+}
